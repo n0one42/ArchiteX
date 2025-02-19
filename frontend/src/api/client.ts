2ff519a6789f25d623a696ae902d0cf35c9b70e9
Alex <n0one42@proton.me>
2025-02-19 14:07:59 +0100

    Update client.ts

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -48,6 +48,8 @@ export interface IClient {
 
     postApiUsersManageInfo(infoRequest: InfoRequest): Promise<InfoResponse>;
 
+    getApiUsersUserRoles(): Promise<RolesEnum[]>;
+
     getWeatherForecasts(): Promise<WeatherForecast[]>;
 }
 
@@ -830,6 +832,40 @@ export class Client implements IClient {
         return Promise.resolve<InfoResponse>(null as any);
     }
 
+    getApiUsersUserRoles(signal?: AbortSignal): Promise<RolesEnum[]> {
+        let url_ = this.baseUrl + "/api/Users/userRoles";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: RequestInit = {
+            method: "GET",
+            signal,
+            headers: {
+                "Accept": "application/json"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processGetApiUsersUserRoles(_response);
+        });
+    }
+
+    protected processGetApiUsersUserRoles(response: Response): Promise<RolesEnum[]> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            result200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver) as RolesEnum[];
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<RolesEnum[]>(null as any);
+    }
+
     getWeatherForecasts(signal?: AbortSignal): Promise<WeatherForecast[]> {
         let url_ = this.baseUrl + "/api/WeatherForecasts";
         url_ = url_.replace(/[?&]$/, "");
@@ -1019,6 +1055,10 @@ export interface InfoRequest {
     newEmail?: string | undefined;
     newPassword?: string | undefined;
     oldPassword?: string | undefined;
+}
+
+export enum RolesEnum {
+    Administrator = "Administrator",
 }
 
 export interface WeatherForecast {
