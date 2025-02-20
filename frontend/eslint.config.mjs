db005979a0c9f1bb32d98daf6a7e4d86a1cb61b0
Alex <n0one42@proton.me>
2025-02-20 13:28:30 +0100

    Update eslint.config.mjs

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,5 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
-// import boundaries from "eslint-plugin-boundaries";
+import boundaries from "eslint-plugin-boundaries";
 import perfectionist from "eslint-plugin-perfectionist";
 import prettier from "eslint-plugin-prettier";
 import { dirname } from "path";
@@ -49,80 +49,79 @@ const prettierIntegration = {
 };
 
 // Custom boundaries config from your old .eslintrc.json
-// const boundariesConfig = {
-//   plugins: {
-//     boundaries,
-//   },
-//   rules: {
-//     ...perfectionist.configs["recommended-natural"].rules,
-//     // ...boundaries.configs.recommended.rules, // TODO: Uncomment at the end of the migration
-//     "boundaries/element-types": [
-//       "error",
-//       {
-//         default: "disallow",
-//         rules: [
-//           {
-//             allow: ["shared"],
-//             from: ["shared"],
-//           },
-//           {
-//             allow: ["shared", ["feature", { featureName: "${from.featureName}" }]],
-//             from: ["feature"],
-//           },
-//           {
-//             allow: ["shared", "feature"],
-//             from: ["app", "neverImport"],
-//           },
-//           {
-//             allow: ["app", "css"],
-//             from: ["app"],
-//           },
-//         ],
-//       },
-//     ],
-//     "boundaries/no-private": "off",
-//     "boundaries/no-unknown": "error",
-//     "boundaries/no-unknown-files": "error",
-//   },
-//   settings: {
-//     "boundaries/elements": [
-//       {
-//         mode: "full",
-//         pattern: ["src/components/**/*", "src/hooks/**/*", "src/lib/**/*"],
-//         type: "shared",
-//       },
-//       {
-//         capture: ["featureName"],
-//         mode: "full",
-//         pattern: ["src/features/*/**/*"],
-//         type: "feature",
-//       },
-//       {
-//         capture: ["_", "fileName"],
-//         mode: "full",
-//         pattern: ["src/app/**/*"],
-//         type: "app",
-//       },
-//       {
-//         mode: "full",
-//         pattern: ["src/*", "src/tasks/**/*"],
-//         type: "neverImport",
-//       },
-//       {
-//         mode: "full",
-//         pattern: ["auth/lib/**/*"],
-//         type: "auth",
-//       },
-//       {
-//         mode: "full",
-//         pattern: ["**/*.css"],
-//         type: "css",
-//       },
-//     ],
-//     // "boundaries/include": ["src/**/*", "!src/mocks/**/*"],
-//     "boundaries/include": ["src/**/*"],
-//   },
-// };
+const boundariesConfig = {
+  plugins: {
+    boundaries,
+  },
+  rules: {
+    ...perfectionist.configs["recommended-natural"].rules,
+    // ...boundaries.configs.recommended.rules, // TODO: Uncomment at the end of the migration
+    "boundaries/element-types": [
+      "error",
+      {
+        default: "disallow",
+        rules: [
+          {
+            allow: ["shared"],
+            from: ["shared"],
+          },
+          {
+            allow: ["shared", ["feature", { featureName: "${from.featureName}" }]],
+            from: ["feature"],
+          },
+          {
+            allow: ["shared", "feature"],
+            from: ["app", "neverImport"],
+          },
+          {
+            allow: ["app", "css"],
+            from: ["app"],
+          },
+        ],
+      },
+    ],
+    "boundaries/no-unknown": "warn",
+    "boundaries/no-unknown-files": "warn",
+  },
+  settings: {
+    "boundaries/elements": [
+      {
+        mode: "full",
+        pattern: ["src/components/**/*", "src/hooks/**/*", "src/lib/**/*"],
+        type: "shared",
+      },
+      {
+        capture: ["featureName"],
+        mode: "full",
+        pattern: ["src/features/*/**/*"],
+        type: "feature",
+      },
+      {
+        capture: ["_", "fileName"],
+        mode: "full",
+        pattern: ["src/app/**/*"],
+        type: "app",
+      },
+      {
+        mode: "full",
+        pattern: ["src/*", "src/tasks/**/*"],
+        type: "neverImport",
+      },
+      {
+        mode: "full",
+        pattern: ["auth/lib/**/*"],
+        type: "auth",
+      },
+      {
+        mode: "full",
+        pattern: ["**/*.css"],
+        type: "css",
+      },
+    ],
+    // "boundaries/include": ["src/**/*", "!src/mocks/**/*"],
+    "boundaries/include": ["src/**/*"],
+  },
+};
 
 const perfectionistConfig = {
   plugins: {
