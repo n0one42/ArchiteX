bff2df84199d3bbdf7797013e236fd42bb6f9f2b
Alex <n0one42@proton.me>
2025-02-25 09:08:09 +0100

    Update Program.cs

--- backend/src/Web/Program.cs
+++ backend/src/Web/Program.cs
@@ -3,35 +3,40 @@ using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
-#if DEBUG
-Console.WriteLine("Debug mode");
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("DevCorsPolicy",
-    policy =>
-    {
-        // IMPORTANT: When using credentials (cookies) you must specify the SetIsOriginAllowed or WithOrigins explicitly.
-        policy.SetIsOriginAllowed(origin => true); // Allow any origin
-                                                   //#if DEBUG
-                                                   //        policy.SetIsOriginAllowed(origin => true); // Allow any origin
-                                                   //#else
-                                                   //        policy.WithOrigins("http://localhost:5142");   // Replace "http://localhost:3000" with your Next.js app URL as needed.
-                                                   //#endif
-        policy.AllowCredentials() // Allow credentials
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
-#endif
-
-
 // Add services to the container.
 builder.AddKeyVaultIfConfigured();
 builder.AddApplicationServices();
 builder.AddInfrastructureServices();
 builder.AddWebServices();
+
+if (builder.Environment.IsDevelopment())
+{
+    Console.WriteLine("Debug mode");
+    builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+    builder.Logging.AddConsole();
+
+}
+
+builder.Services.AddCors(options =>
+{
+    // IMPORTANT: When using credentials (cookies) you must specify the SetIsOriginAllowed or WithOrigins explicitly.
+    options.AddPolicy("DevCorsPolicy",
+    policy =>
+    {
+        policy.SetIsOriginAllowed(origin => true);
+        policy.AllowCredentials()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+
+    options.AddPolicy("ProdCorsPolicy", policy =>
+    {
+        policy.WithOrigins("https://architex-frontend-local.mydom.com", "https://architex-frontend.mydom.com")    // ! Replcae with yours
+              .AllowCredentials()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -42,21 +47,30 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions
     // Optionally configure KnownProxies/KnownNetworks if needed
 });
 
-// Configure the HTTP request pipeline.
-#if DEBUG
-app.UseCors("DevCorsPolicy");
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("DevCorsPolicy");
+}
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+if (app.Environment.IsProduction())
+{
+    app.UseCors("ProdCorsPolicy");
+}
+
 //app.UseCookiePolicy(new CookiePolicyOptions // This is the global one which overrides all the other settings inside DependencyInjection. Mostly not needed.
 // IMPORTANT: you can not use SameSiteMode.None without HTTPS - To make your life easier, always use https and a domain even through /etc/hosts if using cookies
 //{
 //    MinimumSameSitePolicy = SameSiteMode.None,
 //    Secure = CookieSecurePolicy.Always,
 //});
-app.UseExceptionHandler("/Error");
-#else
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-app.UseHsts();
-app.UseHttpsRedirection();
-#endif
+
+
 await app.InitialiseDatabaseAsync();
 
 // Essential for cookie authentication
@@ -64,6 +78,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseHealthChecks("/health");
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseSwaggerUi(settings =>
